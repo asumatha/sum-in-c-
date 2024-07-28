e73fce78e5cb7484ec4579a3d572632cfd156bb6
Uchit joshi <153509841+asumatha@users.noreply.github.com>
2024-07-28 07:23:55 +0545

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+    int main (){
+            int a,b,c;
+                printf("Enter a number a:");
+                    scanf("%d",&a);
+                        printf("Enter a number b :");
+                            scanf("%d",&b);
+                            c=a+b;
+                                printf("The sum of a and b is %d",c);
+
+return 0;
+    }
